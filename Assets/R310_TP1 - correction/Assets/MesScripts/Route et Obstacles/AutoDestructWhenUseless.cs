eb436f714044ba409f47a82d74ae5ced94efd913
JoaDouillard <joachimdouillardd@gmail.com>
2025-01-07 18:14:54 +0100

    oui

--- Assets/R310_TP1 - correction/Assets/MesScripts/Route et Obstacles/AutoDestructWhenUseless.cs
+++ Assets/R310_TP1 - correction/Assets/MesScripts/Route et Obstacles/AutoDestructWhenUseless.cs
@@ -4,12 +4,16 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using UnityEngine;
 
-public class AutoDestructWhenUseless : MonoBehaviour{
-    public Transform    player;
-    public float        maxDist;
+public class AutoDestructWhenUseless : MonoBehaviour
+{
+    public Transform player;
+    public float maxDist;
 
-    void Update(){
+    void Update()
+    {
         if ((player.position.z - transform.position.z) > maxDist)
             Destroy(this.gameObject);
+
     }
+
 }
